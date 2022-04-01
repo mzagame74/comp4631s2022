06cce850c2363413e4ff411c72d08568e28e62ef
Matt <mzagame74@gmail.com>
2022-04-01 16:13:34 -0400

    made ItemAdapter class abstract

--- myPortfolio/app/src/main/java/android/example/myportfolio/ItemAdapter.java
+++ myPortfolio/app/src/main/java/android/example/myportfolio/ItemAdapter.java
@@ -1,13 +1,10 @@
 package android.example.myportfolio;
 
-import android.view.ViewGroup;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+public abstract class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private final List<ItemView> viewList;
 
     public ItemAdapter(List<ItemView> viewList) {
@@ -21,17 +18,6 @@ public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     @Override
     public int getItemViewType(int position) {
         return viewList.get(position).getViewType();
-    }
-
-    @NonNull
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
-    }
-
-    // TODO: figure out how to dynamically handle live data for RecyclerView
-    @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
     }
 
     @Override
